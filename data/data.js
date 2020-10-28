8e853d1883766d27cd40b1668d027eb7c0ba1799
orhanors <orsorhan1@gmail.com>
2020-10-28 13:11:00 +0300

    some edition

--- data/data.js
+++ data/data.js
@@ -1,4 +1,49 @@
-const albums = [{
-    albumName:"",
-    songList:[]
+const playlist = [{
+    name:"",
+    cover:"./assets/playlist_covers/Vintage.jpeg",
+    songList:[],
+},{
+    name:"",
+    cover:"/assets/playlist_covers/Moon.jpeg",
+    songList:[],
+},{
+    name:"",
+    cover:"./assets/playlist_covers/Sweet Gangsta .jpeg",
+    songList:[],
+},{
+    name:"",
+    cover:"./assets/playlist_covers/Tastemakers.jpeg",
+    songList:[],
+},{
+    name:"",
+    cover:"./assets/playlist_covers/At Work.jpeg",
+    songList:[],
+},{
+    name:"",
+    cover:"./assets/playlist_covers/Sunshine .jpeg",
+    songList:[],
+},{
+    name:"",
+    cover:"./assets/playlist_covers/Party.jpeg",
+    songList:[],
+},{
+    name:"",
+    cover:"./assets/playlist_covers/Freedom .jpeg",
+    songList:[],
+},{
+    name:"",
+    cover:"./assets/playlist_covers/Made for you.jpeg",
+    songList:[],
+},{
+    name:"",
+    cover:"./assets/playlist_covers/Kitchen .jpeg",
+    songList:[],
+},{
+    name:"",
+    cover:"./assets/playlist_covers/French Rap .jpeg",
+    songList:[],
+},{
+    name:"",
+    cover:"./assets/playlist_covers/Focus.jpeg",
+    songList:[],
 }]
